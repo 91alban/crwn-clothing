46ba2532bc9df5e20b975dfaf8454743a1d3d490
91alban <aloku93@gmail.com>
2021-11-04 16:34:05 +0100

    header comp

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
+
 import HomePage from './pages/homepage/homepage.jsx';
 import ShopPage from './pages/shop/shop.jsx';
-import './pages/homepage/homepage.scss';
+import Header from './components/header/header.jsx';
 
 
 function App() {
-  return (
-    <div>
-      <HomePage exact path='/' component={HomePage} />
-      <Route path='/shop' component={ShopPage} />
-    </div>
-  );
-} 
+    return ( <
+        div >
+        <
+        Header / >
+        <
+        Switch >
+        <
+        Route exact path = '/'
+        component = { HomePage }
+        />  <
+        Route path = '/shop'
+        component = { ShopPage }
+        />  <
+        /Switch>  <
+        /div>
+    );
+}
 
 export default App;
