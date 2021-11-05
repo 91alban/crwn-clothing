674afb560dd7a0b030ab93284842256e03dc1940
91alban <aloku93@gmail.com>
2021-11-05 12:36:41 +0100

    added sign-in form input

--- src/App.js
+++ src/App.js
@@ -5,25 +5,20 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.jsx';
 import ShopPage from './pages/shop/shop.jsx';
+import LogInPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
 import Header from './components/header/header.jsx';
 
 
 function App() {
-    return ( <
-        div >
-        <
-        Header / >
-        <
-        Switch >
-        <
-        Route exact path = '/'
-        component = { HomePage }
-        />  <
-        Route path = '/shop'
-        component = { ShopPage }
-        />  <
-        /Switch>  <
-        /div>
+    return ( 
+      <div>
+        <Header />
+        <Switch>
+        <Route exact path='/' component={ HomePage }/> 
+        <Route path='/shop' component={ ShopPage }/> 
+        <Route path='/signin' component={ LogInPage }/> 
+        </Switch> 
+      </div>
     );
 }
 
