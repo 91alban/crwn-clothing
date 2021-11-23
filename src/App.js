6ac8ed8cbc3a64d321e3213e8902040e9c1822ba
91alban <aloku93@gmail.com>
2021-11-23 13:36:50 +0100

    integrated firebase, created sign in with google and log out

--- src/App.js
+++ src/App.js
@@ -7,19 +7,44 @@ import HomePage from './pages/homepage/homepage.jsx';
 import ShopPage from './pages/shop/shop.jsx';
 import LogInPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
 import Header from './components/header/header.jsx';
+import { auth } from './firebase/firebase.utils';
 
 
-function App() {
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+       this.setState({ currentUser: user });
+
+       console.log(user);
+     });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+  
+  render() {
     return ( 
-      <div>
-        <Header />
-        <Switch>
-        <Route exact path='/' component={ HomePage }/> 
-        <Route path='/shop' component={ ShopPage }/> 
-        <Route path='/signin' component={ LogInPage }/> 
-        </Switch> 
-      </div>
-    );
+        <div>
+          <Header currentUser={this.state.currentUser} />
+          <Switch>
+          <Route exact path='/' component={ HomePage }/> 
+          <Route path='/shop' component={ ShopPage }/> 
+          <Route path='/signin' component={ LogInPage }/> 
+          </Switch> 
+        </div>
+      );
+  }
 }
 
 export default App;
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,23 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+const config = {
+    apiKey: "AIzaSyCkyI-Am5LDgT7u3-xYHDOXtOT8syAFNU4",
+    authDomain: "crown-clothing-db-c167b.firebaseapp.com",
+    projectId: "crown-clothing-db-c167b",
+    storageBucket: "crown-clothing-db-c167b.appspot.com",
+    messagingSenderId: "755609743734",
+    appId: "1:755609743734:web:3773b767d5ab7ccf450a57"
+  };
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
